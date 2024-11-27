59ad85232a0510e7328c4b5c4af0fc4263354fb2
unknown <maximxcomputer@gmail.com>
2024-11-27 11:06:34 +0300

    addBranch

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log('Hello World');
-
-const userAge = 35;
-if (userAge >= 18) {
-    console.log('Вы совершеннолетний');
-} else {
-    console.log('Вы НЕ совершеннолетний');
-}
+//
+// const userAge = 35;
+// if (userAge >= 18) {
+//     console.log('Вы совершеннолетний');
+// } else {
+//     console.log('Вы НЕ совершеннолетний');
+// }
